f5f229163363177ef6af9819885425f89f8afdc5
unknown <lvova.nikol@mail.ru>
2020-04-05 08:42:16 +0300

    Объявление методов

--- Program.cs
+++ Program.cs
@@ -17,6 +17,73 @@ namespace Converter
                 eur=this.eur;
                 rub=this.rub;
             }
+             public double SomUsd()
+            {
+                return n*usd;
+            }
+            public double SomEur()
+            {
+                return n*eur;
+            }
+            public double SomRub()
+            {
+                return n*rub;
+            }
+            public double UsdSom()
+            {
+                return n/usd;
+            }
+            public double EurSom()
+            {
+                return n/eur;
+            }
+            public double RubSom()
+            {
+                return n/rub;
+            }
+            public void ExchangeRate()
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Курс валют: \nДоллар: {usd} \nЕвро: {eur} \nРубль: {rub}");
+            }
+            public void GetInfo()
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                if (value == "usd")
+                {
+                    Console.WriteLine($"Ваша сумма($): {n}");
+                    Console.WriteLine($"Ваша сумма в сомони: {UsdSom()}");
+                }
+                if (value == "eur")
+                {
+                    Console.WriteLine($"Ваша сумма(Евро): {n}");
+                    Console.WriteLine($"Ваша сумма в сомони: {EurSom()}");
+                }
+                if (value == "rub")
+                {
+                    Console.WriteLine($"Ваша сумма(рубль): {n}");
+                    Console.WriteLine($"Ваша сумма в сомони: {RubSom()}");
+                }
+                if (value == "som")
+                {
+                    Console.WriteLine($"Ваша сумма: {n}");
+                    
+                    switch (val)
+                    {
+                        case "usd": Console.WriteLine($"Ваша сумма в долларах: {SomUsd()}");
+                        break;
+                        case "eur": Console.WriteLine($"Ваша сумма в еврах: {SomEur()}");
+                        break;
+                        case "rub": Console.WriteLine($"Ваша сумма в рублях: {SomRub()}");
+                        break;
+                        default: Console.WriteLine("Вы ввели некорректный текст для обозначения валюты!!!");
+                        break;
+                    }
+                }
+                else Console.WriteLine("Вы ввели некорректный текст для обозначения вашей валюты!!!");
+                  
+            }
+
         }
     class Program
     {
