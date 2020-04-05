f9943226fd0cbef7cddfd0d6087760fcde3a64ac
unknown <lvova.nikol@mail.ru>
2020-04-05 08:27:30 +0300

    Объявление класса, его полей и конструктора

--- Program.cs
+++ Program.cs
@@ -2,6 +2,22 @@
 
 namespace Converter
 {
+    class Converter
+        {
+            public double usd=0.098;
+            public double eur=0.091;
+            public double rub=7.33;
+            public string val, value;
+            public double n;
+            public Converter(){}
+            public Converter(double usd, double eur, double rub)
+            {
+
+                usd=this.usd;
+                eur=this.eur;
+                rub=this.rub;
+            }
+        }
     class Program
     {
         static void Main(string[] args)
